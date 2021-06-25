03fa110588012dab1a9bdb065611548f6a237462
“Naryy” <narikawanariyuki@gmail.com>
2021-06-25 13:55:38 +0900

    add other page

--- html/news.php
+++ html/news.php
@@ -13,9 +13,10 @@
         <nav class="nav_pc">
             <ul>
             <li><a href="./index.html">TOP</a></li>
-                <li><a href="#">NEWS</a></li>
+            <li><a href="./index.html">TOP</a></li>
+                <li><a href="./news.php">NEWS</a></li>
                 <li><a href="./cm.html">CM</a></li>
-                <li><a href="#">AEON商品</a></li>
+                <li><a href="aeonshohin.html">AEON商品</a></li>
                 <li><a href="https://www.aeonfinancial.co.jp/esportsevent" target="_brank">ランディングページ</a></li>
                 <li><a href="#">Twitter</a></li>
                 <li><a href="#">YouTube</a></li>
@@ -23,7 +24,9 @@
         </nav>
     </div>
     <div class="content_wrap">
-
+        <div class="news_wrap">
+            <h1>NEWS</h1>
+        </div>
     </div>
     <!---->
     <div class="footer">
